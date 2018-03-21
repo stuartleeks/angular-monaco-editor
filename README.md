c2069ce852c398e50ba2d263cb764ab32d154da4
Stuart Leeks <stuartle@microsoft.com>
2018-03-21 09:28:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Monaco editor
+
+Work in progress sample to integrate [monaco editor](https://github.com/Microsoft/monaco-editor) with angularjs v1
+
+## TODO
+
+This is an early stage project. There are still some significant pieces of work to be done, including:
+
+* Set up gulp to copy monaco-editor to app/libs/monaco-editor
+* Look for an appropriate event on monaco editor to sync changes to the code back to the angular scope
---
+++ app/monaco-editor/monaco-editor.directive.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular
+  .module('monacoEditor')
+  .directive('monacoEditor', function () {
+
+    function link(scope, element, attrs) {
+      var editorElement = element[0];
+      require(['vs/editor/editor.main'], function () {
+        var editor = monaco.editor.create(editorElement, {
+          value: scope.code,
+          language: scope.language
+        });
+        // TODO - look up api docs to find a suitable event to handle as the onDidChangeModelContent event only seems to fire for certain changes!
+        // As a fallback, currently updating scope on a timer...
+        // editor.onDidChangeModelContent = function(e){
+        //   console.log('modelContent changed');
+        //   scope.code = editor.getValue();
+        //   scope.$apply();
+        // }
+        function updateScope() {
+          scope.code = editor.getValue();
+          scope.$apply();
+        };
+        setInterval(updateScope, 1000); // TODO - need to clear the interval when the directive is torn down
+      });
+    }
+
+    return {
+      link: link,
+      scope: {
+        code: '=code',
+        language: '=language'
+      }
+    };
+  });
